629d03b4c56a6648c4a9bdf93ba5b71bc79c8782
501306162 <501306162@users.noreply.github.com>
2017-05-20 16:04:36 +0800

    Add files via upload

--- Metropolis_texturer.m
+++ Metropolis_texturer.m
@@ -1,41 +1,49 @@
 clc
-clear all
+clear 
 close all
 
-label_num=3;
-label=randi(1,128,128);
-range =1; %3*3
+label_num=4;
+% Figure 3.7 :(1)
+label=randi(label_num,128,128);
+% label_f=zeros(128,128);
+figure,
+imshow(label,[]);
 alpha=0;
-beta=[1,1,1,-1];
+beta=[1,1,1,1];
 maxiter=50;
 iter=0;
-
+T=1; 
 label_f=label;
-% zeros(100,100);
 while iter<maxiter
     for ind=1:numel(label_f)
-        % Figure 3.7 :(2.1)
+        % Figure 3.7 :(2.1.1)
         d=ind;
-        label_f(d)=randi(label_num);
-        l_i=label_f(d);
+
         %当前点的邻域 & 邻域标记
         neigh_ind=neighborhood(d,label_f,8);
         l_n=label_f(neigh_ind);
-%         label_f(neigh_ind)=l_n;
+        
+         % Figure 3.7 :(2.1.2)
+        l_i=label_f(d);
         
         %计算能量 U=V1+sum(V2) :(2.2)
         U_i=MLL(d,alpha,neigh_ind,beta,label_f);
-    %     U_n=zeros(1,numel(neigh_ind));
-    %     for k=1:numel(neigh_ind)
-        k=randi(numel(neigh_ind));
-        label_f(d)=l_n(k);
+
+        %随机选取一个标签计算能量
+            k=0;
+        while label_f(d)==l_i
+            k =randi(label_num);
+            label_f(d)=k;
+         end
         U_n=MLL(d,alpha,neigh_ind,beta,label_f);
-    %     end
+
+        %判断是否满足接受概率
+        label_f(d)=l_i;
+        
         U_e=U_n-U_i;
         P=min(1,exp(-U_e));
         if rand(1)<P
-            label_f(d)=l_n(k);
-        else label_f(d)=l_i;
+            label_f(d)=k;
         end
     end
     iter=iter+1;
